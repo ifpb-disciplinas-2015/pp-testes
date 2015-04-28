1c6239202f5f795fd0ec7071713cc675d8a825a1
João Marcos F <joaomarccos.ads@gmail.com>
2015-04-28 09:07:59 -0300

    Refactoring testes com raiz quadrada

--- src/test/java/ifpb/pp/testes/CalculatorTestRaiz.java
+++ src/test/java/ifpb/pp/testes/CalculatorTestRaiz.java
@@ -1,6 +1,8 @@
 package ifpb.pp.testes;
 
-import ifpb.pp.teste.Calculadora;
+import ifpb.pp.teste.CalculadoraException;
+import ifpb.pp.teste.CalculadoraRaizQuadrada;
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 /**
@@ -8,20 +10,23 @@ import org.junit.Test;
  * @author João Marcos F <joaomarccos.ads@gmail.com>
  */
 public class CalculatorTestRaiz {
+    
+    CalculadoraRaizQuadrada calc = new CalculadoraRaizQuadrada();
+    
     @Test
     public void testaRaizQuadrada() {
-        assertEquals("Resultado válido", 2, Calculadora.raizQuadrada(4));
-        assertTrue("Resultado válido", 3==Calculadora.raizQuadrada(9));
-        assertEquals("Resutado válido", 0, Calculadora.raizQuadrada(0));
+        assertEquals("Resultado válido", 2, calc.raizQuadrada(4));
+        assertTrue("Resultado válido", 3==calc.raizQuadrada(9));
+        assertEquals("Resutado válido", 0, calc.raizQuadrada(0));
     }
 
     @Test(expected = CalculadoraException.class)
     public void testaRaizQuadradaNegativos() {
-        assertEquals("Impossivel calcular raiz de numeros negativos", 2, Calculadora.raizQuadrada(-1));
+        assertEquals("Impossivel calcular raiz de numeros negativos", 2, calc.raizQuadrada(-1));
     }
     
     @Test(expected = CalculadoraException.class)
     public void testaRaizQuadradaNulos() {
-        assertEquals("Impossivel calcular raiz de um valor nulo", 2, Calculadora.raizQuadrada(null));
+        assertEquals("Impossivel calcular raiz de um valor nulo", 2, calc.raizQuadrada(null));
     }    
 }
